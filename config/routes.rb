3491f3a4eea4a510d9712ae908cfa41412d85f89
sheamf <miles_shea@yahoo.com>
2016-03-25 20:16:46 -0600

    add products controller, actions, routes, and tests

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
                   constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, only: [:show, :create, :update, :destroy]
       resources :sessions, only: [:create, :destroy]
+      resources :products, only: [:index, :show]
     end
 
   end
