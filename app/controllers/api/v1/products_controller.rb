3ef684d15553d605ff6d4b66dbe5a832e16b9350
sheamf <miles_shea@yahoo.com>
2016-03-25 21:29:46 -0600

    add create, update, and destroy resources and tests for auth'd users

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ProductsController < ApplicationController
   respond_to :json
 
+  before_action :authenticate_with_token!, only: [:create, :update, :destroy]
+
   def index
     respond_with Product.all
   end
@@ -8,5 +10,39 @@ class Api::V1::ProductsController < ApplicationController
   def show
     respond_with Product.find(params[:id])
   end
+
+  def create
+    product = current_user.products.build(product_params)
+    # since product_params contains a user_id, it's not necessary to build it through the user
+    # assoc, but the author suggested that this way is more descriptive
+
+    if product.save
+      render json: product, status: 201, location: [:api, product]
+    else
+      render json: { errors: product.errors }, status: 422
+    end
+  end
+
+  def update
+    product = current_user.products.find(params[:id])
+
+    if product.update(product_params)
+      render json: product, status: 200, location: [:api, product]
+    else
+      render json: { errors: product.errors }, status: 422
+    end
+  end
+
+  def destroy
+    product = current_user.products.find(params[:id])
+    product.destroy
+    head 204
+  end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:title, :price, :published)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
 
     scope module: :v1, 
                   constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :users, only: [:show, :create, :update, :destroy]
+      resources :users, only: [:show, :create, :update, :destroy] do
+        resources :products, only: [:create, :update, :destroy]
+      end
       resources :sessions, only: [:create, :destroy]
       resources :products, only: [:index, :show]
     end
