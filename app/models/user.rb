9a5cdf2fe92656794d0b86a5e3ca92d34e2df1ea
sheamf <miles_shea@yahoo.com>
2016-03-26 01:34:57 -0600

    add user relationship to order + test

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :products, dependent: :destroy
+  has_many :orders, dependent: :destroy
 
   validates :auth_token, uniqueness: true
 
